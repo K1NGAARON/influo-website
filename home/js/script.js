0fa1c2c69826ed440cbe9e27a8f9c69b5fcfe45a
K1NGAARON <gerardaaron1997@gmail.com>
2023-07-25 15:25:19 +0200

    changes

--- home/js/script.js
+++ home/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $(".expand").hover(
         function() {
+            $(this).addClass('active');
             $(this).find(".item-content").addClass("active");
         },
         function() {
+            $(this).removeClass('active');
             $(this).find(".item-content").removeClass("active");
         }
     );
